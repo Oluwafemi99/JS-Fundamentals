4020c368bac00f7c47b54912628261971cc619a2
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-06 10:37:48 +0100

    multi languages added

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+// defining a lang variable
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// iterating through the variable
+for (i=0; i<lang.length; i++) {
+    console.log(lang[i]);
+}
