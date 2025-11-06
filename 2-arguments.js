2ef8030b913d3bcd810f4b4ae0900ac8a0a65702
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-06 11:20:53 +0100

    arguments added

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,8 +3,8 @@ const args = process.argv.slice(2);
 // using if condintion to check for arguments
 if (args.length == 0) {
     console.log('No argument');
-}else if (args.length >= 1) {
-    console.log('Arguments found')
+}else if (args.length == 1) {
+    console.log('Argument found')
 }else{
     console.log('Arguments found')
 }
