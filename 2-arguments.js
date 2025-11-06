653f02627c632f55b3c4b5d8cc29f034940f75db
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-06 11:06:11 +0100

    arguments added

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// initializing arguments
+let args = []
+// using if condintion to check for arguments
+if (args.length == 0) {
+    console.log('No argument');
+}else if (args.length >= 1) {
+    console.log('Arguments found')
+}else{
+    console.log('Arguments found')
+}
