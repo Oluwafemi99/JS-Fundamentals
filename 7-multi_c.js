bf4ff2fced97a6574964ec0139ec3eb85424ae4f
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-06 14:56:13 +0100

    7 multi added

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const x = process.argv.slice(2);
+
+let num = Number(x);
+
+if (isNaN(num)) {
+    console.log('Missing number of occurrences')
+}else {
+    for (i=0; i<=num; i++) {
+        console.log('C is fun')
+    }
+}
