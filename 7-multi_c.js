b119dba2765e629dc62cfea22f082600aae5c865
oluwafemi <oluwafemidojumo@gmail.com>
2025-11-06 14:58:35 +0100

    7 multi added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -5,7 +5,7 @@ let num = Number(x);
 if (isNaN(num)) {
     console.log('Missing number of occurrences')
 }else {
-    for (i=0; i<=num; i++) {
+    for (i=0; i<num; i++) {
         console.log('C is fun')
     }
 }
